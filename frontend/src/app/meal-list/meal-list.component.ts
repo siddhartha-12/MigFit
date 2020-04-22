c6506dfa95f91a81c5da9b8ae7a41ef096b8c2d6
Avani Iddalgi <iddalgi.a@husky.neu.edu>
2020-04-22 09:59:15 -0400

    Download as PDF

--- frontend/src/app/meal-list/meal-list.component.ts
+++ frontend/src/app/meal-list/meal-list.component.ts
@@ -2,6 +2,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MealService } from '../services/meals.service';
 import { UserService } from 'src/app/services/user.service';
+import * as jspdf from 'jspdf';  
+import html2canvas from 'html2canvas'; 
+
 
 @Component({
   selector: 'app-meal-list',
@@ -44,4 +47,23 @@ export class MealListComponent implements OnInit {
       this.getAllMeals(localStorage.getItem('userId'));
     });
   }
+
+ //to download the contents of page as pdf file
+ public captureScreen()  
+  {  
+    let data = document.getElementById('contentToConvert');  
+    html2canvas(data).then(canvas => {  
+      // Few necessary setting options  
+      var imgWidth = 208;   
+      var pageHeight = 295;    
+      var imgHeight = canvas.height * imgWidth / canvas.width;  
+      var heightLeft = imgHeight;  
+  
+      const contentDataURL = canvas.toDataURL('http://localhost:4200/list')  
+      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
+      var position = 0;  
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+      pdf.save('Meal.pdf'); // Generated PDF   
+    });
+}
 }
